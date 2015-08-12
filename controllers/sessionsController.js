907e7b38984ed332976acb450929ca2d114455c7
hews <p@hews.co>
2015-08-12 14:33:40 -0700

    refactor routes

---
+++ controllers/sessionsController.js
@@ -0,0 +1,25 @@
+var passport = require('passport');
+
+var User = require('../models/User');
+
+var newRoute = function(req, res) {
+  res.render('sessions/new', {user : req.user});
+};
+
+var create = function(req, res, next) {
+  req.session.save(function (err) {
+    if (err) return next(err);
+    res.redirect('/');
+  });
+};
+
+var destroy = function(req, res) {
+  req.logout();
+  res.redirect('/');
+};
+
+module.exports = {
+  new:     newRoute,
+  create:  create,
+  destroy: destroy
+};
--- routes/index.js
+++ routes/index.js
@@ -1,64 +1,41 @@
 var express = require('express');
 var passport = require('passport');
-var User = require('../models/User');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res) {
-  res.render('index', {user: req.user});
-});
+var usersController    = require('../controllers/usersController');
+var sessionsController = require('../controllers/sessionsController');
 
-router.get('/register', function (req, res) {
-  res.render('auth/register');
-});
+/*
+ * Passport auth helpers.
+ */
 
-router.post('/register', function (req, res) {
-  User.register(new User({username: req.body.username, name: req.body.name}), req.body.password, function(err, user) {
-    if (err) return res.render('auth/register', {user: user});
-    passport.authenticate('local')(req, res, function () {
-      req.session.save(function (err) {
-        if (err) {
-          return next(err);
-        }
-        res.redirect('/');
-      });
-    });
-  });
-});
-
-router.get('/login', function (req, res) {
-  res.render('auth/login', {user : req.user});
-});
-
-router.post('/login', passport.authenticate(
+var authenticateUser = passport.authenticate(
   'local',
-  {
-    failureRedirect: '/login'
-  }),
-  function (req, res, next) {
-    req.session.save(function (err) {
-      if (err) return next(err);
-      res.redirect('/');
-    });
-  }
+  {failureRedirect: '/login'}
 );
 
-router.get('/logout', function (req, res) {
-  req.logout();
-  res.redirect('/');
+var isLoggedIn = function(req, res, next) {
+  if (!req.isAuthenticated()) res.redirect('/login');
+  return next();
+};
+
+/*
+ * Define routes.
+ */
+
+router.get('/', function (req, res) {
+  res.render('index', {user: req.user});
 });
 
 router.get('/secret', isLoggedIn, function (req, res) {
   res.render('secret', {user: req.user});
 });
 
-// middleware to make sure a user is logged in
-function isLoggedIn(req, res, next) {
-  // if user is authenticated in the session, carry on 
-  if (req.isAuthenticated())
-    return next();
-  // if they aren't redirect them to the login page
-  res.redirect('/login');
-}
+router.get( '/register', usersController.new);
+router.post('/register', usersController.create);
+
+router.get( '/login',  sessionsController.new);
+router.post('/login',  authenticateUser, sessionsController.create);
+router.get( '/logout', sessionsController.destroy);
 
 module.exports = router;
